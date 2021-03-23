fde02280b642ce1db355baf9161f66a895db71bc
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-03-23 16:51:29 -0700

    added when function

--- assets/js/app.js
+++ assets/js/app.js
@@ -134,9 +134,23 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself'
+            message: 'Provide some information about yourself',
+            // incorporating the "when" property -- 9.3.6
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
